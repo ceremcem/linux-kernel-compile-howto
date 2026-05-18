34a8872736d61d6778abdbcef0c96378f14e456b
Cerem Cem ASLAN <cem@aktos.io>
2026-05-18 16:18:37 +0300

    improved compile time display

--- compile-kernel.sh
+++ compile-kernel.sh
@@ -2,5 +2,5 @@
 
 cca-suspend --disable-by Linux_kernel_compile
 cd linux
-TIMEFORMAT="Elapsed time: %R"; time make -j$(nproc) bindeb-pkg
+TIMEFORMAT="Elapsed time: %E"; time make -j$(nproc) bindeb-pkg
 cca-suspend --enable-for Linux_kernel_compile
