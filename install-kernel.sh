182b0d2575814bb8077d18a83807758577aa286f
Cerem Cem ASLAN <cem@aktos.io>
2026-05-18 13:26:40 +0300

    added other generated files to move list

--- install-kernel.sh
+++ install-kernel.sh
@@ -3,7 +3,8 @@ set -eu
 
 latest=$(cd linux && git describe --tags --abbrev=0)
 version=${latest#v}
-mv *.deb deb 2> /dev/null || true
+mkdir -p deb
+mv *.deb *.buildinfo *.changes deb/ 2> /dev/null || true
 cd deb
 image=$(ls | grep $version | grep image | grep -v dbg)
 header=$(ls | grep $version | grep header)
