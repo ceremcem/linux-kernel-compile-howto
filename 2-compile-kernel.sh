474ef43fe31a63a95abd807b0fb5d05fc35d80a0
Cerem Cem ASLAN <cem@aktos.io>
2026-05-23 18:29:38 +0300

    discarded unnecessary deb pakages for speedup. attempt to use ccache

--- 2-compile-kernel.sh
+++ 2-compile-kernel.sh
@@ -2,5 +2,7 @@
 
 cca-suspend --disable-by Linux_kernel_compile
 cd linux
+export CC="ccache gcc" # sudo apt install ccache
+export KDEB_DISABLE_DEBUG=1 # no debug-*.deb generation
 TIMEFORMAT="Elapsed time: %E"; time make -j$(nproc) bindeb-pkg
 cca-suspend --enable-for Linux_kernel_compile
