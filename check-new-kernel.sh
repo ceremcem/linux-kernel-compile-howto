ff41b5687436bbcba8c9317765cc614acaca9e82
Cerem Cem ASLAN <cem@aktos.io>
2026-06-09 18:34:33 +0300

    improved popup, console output and format

--- check-new-kernel.sh
+++ check-new-kernel.sh
@@ -5,11 +5,19 @@ now() {
     date +%s
 }
 
+datef(){
+    date '+%Y-%m-%d %H:%M'
+}
+
+echolog() {
+    echo -e "\e[32m$(datef)\e[0m: $*"
+}
+
 first_msg_shown=false
 retry_connect=false
 latest_informed_kernel=
-reset_period=$((48 * 3600)) # seconds
-last_reset=$(now)
+reset_period=$((48 * 3600)) # Show info popup in every 48 hours
+last_reset=
 
 while :; do
     while :; do
@@ -31,20 +39,21 @@ while :; do
 
     if [[ "$LATEST_KERNEL" != "$(uname -r)" ]]; then
         if [[ "$LATEST_KERNEL" != "$latest_informed_kernel" ]]; then
-            notify-send -u critical "New Stable Kernel" "New kernel available: $LATEST_KERNEL"
-            echo "New stable kernel available: $LATEST_KERNEL"
+            notify-send -u critical "New Stable Kernel" "New kernel available: $LATEST_KERNEL \n\n$(datef)"
+            echolog "New stable kernel available: $LATEST_KERNEL"
             echo "Changelog: $CHANGELOG_URL"
             latest_informed_kernel="$LATEST_KERNEL"
+            last_reset=$(now)
         fi
     else
         if ! $first_msg_shown; then
-            echo "System is already running on latest stable kernel: v$(uname -r)"
+            echolog "System is already running on latest stable kernel: v$(uname -r)"
             echo "Changelog: $CHANGELOG_URL"
             first_msg_shown=true
         fi
     fi
 
-    # Reset info popup in every 24h
+    # Reset info popup
     if [[ $(now) -gt $((last_reset + reset_period)) ]]; then
         latest_informed_kernel=
         last_reset=$(now)
